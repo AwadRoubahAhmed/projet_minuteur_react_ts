f6b40e68f55436eee0270c5cabae5014aa456504
AwadRoubahAhmed <awadroubahahmed@gmail.com>
2025-01-06 23:04:20 +0100

    Commit project with react typescript...

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Minuteur from "./components/Minuteur";
+
+export default function App() {
+  return (
+    <div
+      className="
+      w-60
+    bg-neutral-200 text-black shadow-2xl rounded-md m-auto mt-52 py-2"
+    >
+      <Minuteur />
+    </div>
+  );
+}
---
+++ src/components/Minuteur.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+const Minuteur = () => {
+  // State;
+  const [count, setCount] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  //Comportement;
+  useEffect(() => {
+    let timer: number;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setCount((prevCount) => prevCount + 1);
+      }, 1000);
+
+      return () => clearInterval(timer);
+    }
+  }, [isRunning]);
+
+  const toggleCounter = () => {
+    setIsRunning(!isRunning);
+  };
+
+  //Affichage(render);
+  return (
+    <div>
+      <h1 className="text-xl font-semibold text-center">MINUTEUR</h1>
+      <div className="">
+        <div className="text-center text-black text-lg mt-2 mx-auto p-2 bg-slate-400 w-24 rounded-md shadow-xl">
+          {count}
+        </div>
+        <button
+          onClick={() => toggleCounter()}
+          className="w-40 btn btn-primary text-white text-base mt-4 ml-10"
+        >
+          {isRunning ? `Démarer` : `Arreter`}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Minuteur;
